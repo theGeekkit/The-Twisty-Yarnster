e08ab959c09ca30d8dea99d48f98e70aee313b97
theGeekkit <sonofgondolin16@gmail.com>
2022-12-17 13:11:07 -0600

    12-17-22

--- src/app/authentication.service.spec.ts
+++ src/app/authentication.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthenticationService } from './authentication.service';
+import { AuthenticationClient } from './authentication.service';
 
 describe('AuthenticationService', () => {
-  let service: AuthenticationService;
+  let service: AuthenticationClient;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthenticationService);
+    service = TestBed.inject(AuthenticationClient);
   });
 
   it('should be created', () => {
